0c61ba62310af3df941358cd7c162df5e32f27c5
James White <james@thewhites.co>
2014-11-17 23:36:16 -0600

    Added db connection logic
    First implementation of a `connect()` method for both MSSQL and
    WebSQL. Untested and therefore likely to be buggy.

--- js/common/mssql/index.js
+++ js/common/mssql/index.js
@@ -1,4 +1,27 @@
-common.mssql = {};
+common.mssql = {
+    config : {
+        user : 'CCM-ADMIN',
+        password : 'c#99jKlw,llAS',
+        server : 'LOCALHOST\\CCMSERVER',
+        database : 'HappyFaces',
+        options : {
+            requestTimeout : 1000000
+        }
+    },
+    db : require('mssql')
+};
+
+
+
+common.mssql.connect = function connect() {
+    return new Promise(function (resolve, reject) {
+        common.mssql.db.connect(common.mssql.config, function (error) {
+            if (error) reject(error);
+            else resolve();
+        });
+    });
+};
+
 
 
 require('./mssqlerror');
--- js/common/websql/index.js
+++ js/common/websql/index.js
@@ -1,4 +1,32 @@
-common.websql = {};
+common.websql = {
+    config : (function () {
+        var serial = localStorage.getItem('websql-config'), config;
+        if (serial) {
+            config = serial ? JSON.parse(serial)
+        } else {
+            config = {
+                name : 'happy-faces',
+                version : '1.0',
+                desc : 'Happy Faces Title XX storage'
+                size : 10 * 1024 * 1024  // 10MB
+            };
+            localStorage.setItem('websql-config', JSON.serialize(config));
+        }
+        return config;
+    })()
+};
+
+
+
+common.websql.connect = function connect() {
+    common.websql.db = openDatabase(
+        common.websql.config.name,
+        common.websql.config.version,
+        common.websql.config.desc,
+        common.websql.config.size
+    );
+};
+
 
 
 require('./websqlerror.js');
