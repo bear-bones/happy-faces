741c06eadd1b2976af96ca4f8eda81d2b922f9b5
James White <james@thewhites.co>
2014-11-28 22:55:34 -0600

    Attendance calculations by month
    Model now calculates and stores attendance data by month for each
    child. Attendance unit is minutes or days, depending on the child's
    config.

--- js/title-xx/model/index.js
+++ js/title-xx/model/index.js
@@ -72,7 +72,11 @@ function* read_ccm() {
     console.log('1st punch: ', punches[0]);
 
 
-    title_xx.model.children = children;
+    title_xx.model.data = children.map(function (child) {return {
+        child_id : child.childkey,
+        name : child.last + ', ' + child.first + ' ' + (child.middle || ''),
+        age : get_age(child.dob)
+    }});
     title_xx.model.punches = punches;
 }
 
@@ -81,19 +85,14 @@ function* read_ccm() {
 function* write_local() {
     console.log('Saving child information to reporting database...');
     try {
-        var fields = title_xx.model.children.map(function (child) {return {
-                child_id : child.childkey,
-                name : child.last + ', ' + child.first + ' ' + child.middle,
-                age : get_age(child.dob)
-            }}),
-            exists = yield title_xx.websql.children.exists(fields);
+        var exists = yield title_xx.websql.children.exists(title_xx.model.data);
         yield title_xx.websql.children.create_all(
-            fields.filter(function (_, key) {return !exists[key]})
+            title_xx.model.data.filter(function (_, key) {return !exists[key]})
         );
         yield title_xx.websql.children.update_all(
-            fields.filter(function (_, key) {return exists[key]})
+            title_xx.model.data.filter(function (_, key) {return exists[key]})
         );
-        yield title_xx.websql.children.delete_except(fields);
+        yield title_xx.websql.children.delete_except(title_xx.model.data);
     } catch (error) {
         console.error(error);
         throw new title_xx.model.ModelError('Error saving child information into reporting database');
@@ -106,7 +105,8 @@ function* write_local() {
         yield title_xx.websql.punches.create_all(
             title_xx.model.punches.map(function (value, key) {return {
                 punch_id : value.pkChildTime, child_id : value.fkChild,
-                time_in : value.dtTimeIn, time_out : value.dtTimeOut
+                time_in : value.dtTimeIn && value.dtTimeIn.getTime(),
+                time_out : value.dtTimeOut && value.dtTimeOut.getTime()
             }})
         );
     } catch (error) {
@@ -119,6 +119,71 @@ function* write_local() {
 
 
 function* process_data(row_id) {
+    var data = title_xx.model.data;
+
+
+    try {
+        title_xx.model.config = {};
+        var config = yield title_xx.websql.config.read(),
+            date = new Date();
+        Object.keys(config).forEach(function (key) {
+            title_xx.model.config[key] = config[key];
+        });
+        if (date.getMonth() > title_xx.model.config.title_xx_year_start)
+            date.setMonth(title_xx.model.config.title_xx_year_start, 1);
+        else
+            date.setMonth(12 - title_xx.model.config.title_xx_year_start, 1);
+        title_xx.model.config.title_xx_year_start = date.getTime();
+    } catch (error) {
+        console.error(error);
+        throw new title_xx.model.ModelError('Error loading app configuration');
+    }
+
+
+    console.log('Processing child punches...');
+    try {
+        for (var i = 0, length = data.length; i < length; ++i) {
+            var child = data[i];
+            child.punches = yield title_xx.websql.punches.read(
+                child.child_id, title_xx.model.config.title_xx_year_start
+            );
+            child.time = new Array(12);
+            child.time.fill(0);
+
+            if (child.auth_unit === 'days') {
+                time = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+                for (var j = 0, len = child.punches.length; j < len; ++j) {
+                    var punch = child.punches[j],
+                        date = new Date(punch.time_in),
+                        month = date.getMonth(), day = '' + date.getDate();
+                    if (!(day in time[month])) {
+                        time[month][day] = true;
+                        ++child.time[month];
+                    }
+                }
+            } else {
+                for (var j = 0, len = child.punches.length; j < len; ++j) {
+                    var punch = child.punches[j];
+                    child.time[(new Date(punch.time_in)).getMonth()]
+                        += (punch.time_out - punch.time_in) / 3600000;
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        throw new title_xx.model.ModelError('Error loading punch information per child');
+    }
+    console.log('All children processed.');
+    for (var i = 0, length = data.length; i < length; ++i) {
+        if (data[i].punches.length > 0) {
+            data[i].punches = data[i].punches.map(function (punch) {
+                return (new Date(punch.time_in)).toString() + ' '
+                    + (new Date(punch.time_out)).toString();
+            });
+            console.log('1st child with punches: ', data[i]);
+            break;
+        }
+    }
 }
 
 
