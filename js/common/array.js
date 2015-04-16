b191ce31f507a349eab0f3f4da4a3e89a2f28d4b
James White <jwhite@ccccorp.com>
2015-04-15 23:31:52 -0500

    nwjs 32bit missing array fill
    shim [].fill because it's missing from 32-bit nwjs (despite being in
    64-bit)

--- js/common/array.js
+++ js/common/array.js
@@ -1,5 +1,8 @@
+// 32-bit nwjs is missing [].fill
 window.Array.prototype.fill = global.Array.prototype.fill =
     [].fill || function (value) {
-        for (var i = 0, length = this.length; i < length; ++i)
+        for (var i = 0, length = this.length; i < length; ++i) {
             this[i] = value;
+        }
+        return this;
     };
